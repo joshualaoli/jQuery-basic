4086ad8bcb33aee1c9d322ff3217be65eb3efb79
Joshua Laoli <joe.pixarts@gmail.com>
2023-05-15 19:18:04 +0700

    how to check jQuery

--- script.js
+++ script.js
@@ -1,25 +1,10 @@
-/**
-
-Manipulating text 
-$ (`p`).addClass (`paragraph margin50`);
-
-$ (`h1`).text(`Hey bro!`);
-
-$ (`button`).html (`<em>click here!</em>`);
-
-$(`img`).attr (`src`, `./Assets/images/crash.png`);
-
-$(`a`). attr (`href`, `https://www.yahoo.com/`);
-
-----------adding event listener with jQuery---------
-
-$ (`button`).click (function() {
-    $(`h1`).css (`color`, `red`);
-})
-
-$(`input`).keypress(function(event) {
-    console.log(event.key)
-})
+/** 
+// untuk mengecek jQuery aktif tidaknya
+$(document).ready(function () {
+    $(`h1`).css(`color`, `blue`);
+});
 */
 
-$
+//Manipulating styles with jQuery
+
+
