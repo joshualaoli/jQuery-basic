99ccbae37cf4dfe075f1957d6bfa904e2a76cacc
Joshua Laoli <joe.pixarts@gmail.com>
2023-05-16 09:30:35 +0700

    deep dive jQuery in the third day 16/05/2022

--- script.js
+++ script.js
@@ -3,8 +3,30 @@
 $(document).ready(function () {
     $(`h1`).css(`color`, `blue`);
 });
+
 */
 
-//Manipulating styles with jQuery
+$(`h1`).addClass(`big-title margin-50`) // .addClass jika ingin menambahkan class sebanyak mungkin
+
+$ (`h1`).text (`Hi, Joshua!`) // .text untuk mengubah text 
+
+$ (`button`). html (`<em> Let's connect! </em>`) // .html  untuk mengubah sifat html
+
+$ (`a`). attr (`href`, `https://www.linkedin.com/`) // .attr yaitu untuk mengubah asal menjadi tujuan
+
+$ (`button`). click (function() {
+    $ (`h1`).css(`color`, `green`); // saat click button, akan muncul warna hijau di h1
+});
+
+$ (`input`). keypress (function(event) {
+    $ (`h1`).text(event.key); // saat pencet input dan mengetik (keypress), h1 akan melakukan perintah yaitu mengganti text 
+})
+
+$ (`h1`). on (`click` , function () {
+    $ (`h1`). css (`color` , `brown`); // saat h1 di klik, maka h1 akan berganti warna menjadi brown
+})
+
+
+
 
 
